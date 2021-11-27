ff20e5a22f846d407788ec75f2d5bcc7355befb0
smilaya <smilaya0@gmail.com>
2021-11-27 15:27:32 +0300

    ADD 14hw

--- hw_14/src/app/index.js
+++ hw_14/src/app/index.js
@@ -32,73 +32,122 @@ const DATA = {
   },
 };
 
+const userFabric = function (firstName, lastName, year, month, day) {
+  return {
+    firstName,
+    lastName,
+    year,
+    month, // 1...12
+    day,
+    get fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
 
+    set fullName(value) {
+      const firstName = value.split(" ")[0];
+      const lastName = value.split(" ")[1];
 
+      if (firstName) {
+        this.firstName = firstName;
+      }
 
-const addUser = function (firstName, lastName, year, month, day) {
-      const FIRST_NAME_MIN_LENGTH = 5;
+      if (lastName) {
+        this.lastName = lastName;
+      }
+    },
+
+    get age() {
+      const month = this.month - 1;
+
+      const now = new Date();
+
+      const originBirthDay = new Date(this.year, month, this.day);
+      const birthDay = new Date(now.getFullYear(), month, this.day);
+
+      const fullAge = birthDay.getFullYear() - originBirthDay.getFullYear();
+
+      if (now > birthDay) {
+        return fullAge;
+      } else {
+        return fullAge - 1;
+      }
+    },
+  };
+};
+
+function addUser() {
+  const entries = [];
+
+  const FIRST_NAME_MIN_LENGTH = 5;
   const FIRST_NAME_MAX_LENGTH = 20;
+
   const validateFirstName = (input) => {
-      return input < FIRST_NAME_MIN_LENGTH || input > FIRST_NAME_MAX_LENGTH;
-    },
-    const minMonth = 1;
-    const maxMonth = 12;
-    const validateMonth = function (input) {
-      return input < minMonth || input > maxMonth;
-    };
-    const minYear = 1900;
-    const date = new Date();
-    const maxYear = date.getFullYear();
-    const validateYear = function (input) {
-      return input < minYear || input > maxYear;
-    },
-    const minDay = 1;
-    const maxDay = DATA.getMaxDay(year, month);
-    const validateDay = function (input) {
-      return input < minDay || input > maxDay;
-    };
-    return{
-        firstName = STDIN.string(
-            `Enter your first name :  min symbol: ${FIRST_NAME_MIN_LENGTH}, max symbols: ${FIRST_NAME_MAX_LENGTH}`,
-            validateFirstName
-          ),
-          lastName = STDIN.string(
-            `Enter your last name :  min symbol: ${FIRST_NAME_MIN_LENGTH}, max symbols: ${FIRST_NAME_MAX_LENGTH}`,
-            validateFirstName
-          ),
-       
-           year = STDIN.number(
-            `Enter your year of birthday : only numbers, min: ${minYear}, max: ${maxYear}`,
-            validateYear
-          ),
-           month = STDIN.number(
-            `Enter your month of birthday : only numbers, min: ${minMonth}, max: ${maxMonth}`,
-            validateMonth
-          ),
+    return input < FIRST_NAME_MIN_LENGTH || input > FIRST_NAME_MAX_LENGTH;
+  };
+  const firstName = STDIN.string(
+    `Enter your first name :  min symbol: ${FIRST_NAME_MIN_LENGTH}, max symbols: ${FIRST_NAME_MAX_LENGTH}`,
+    validateFirstName
+  );
 
-           day = STDIN.number(
-            `Enter your day of birthday : only numbers, min: ${minDay}, max: ${maxDay}`,
-            validateDay
-          ),
-          get age() {
-            const month = this.month - 1;
-      
-            const now = new Date();
-      
-            const originBirthDay = new Date(this.year, month, this.day);
-            const birthDay = new Date(now.getFullYear(), month, this.day);
-      
-            const fullAge = birthDay.getFullYear() - originBirthDay.getFullYear();
-      
-            if (now > birthDay) {
-              return fullAge;
-            } else {
-              return fullAge - 1;
-            }
-          },
-        };
-      };
-      
+  const lastName = STDIN.string(
+    `Enter your last name :  min symbol: ${FIRST_NAME_MIN_LENGTH}, max symbols: ${FIRST_NAME_MAX_LENGTH}`,
+    validateFirstName
+  );
+  const minYear = 1900;
+  const date = new Date();
+  const maxYear = date.getFullYear();
+  const validateYear = function (input) {
+    return input < minYear || input > maxYear;
+  };
+  const year = STDIN.number(
+    `Enter your year of birthday : only numbers, min: ${minYear}, max: ${maxYear}`,
+    validateYear
+  );
 
-      const user = userFabric(firstName, lastName, year, month, day);
-      console.log(user);
+  const minMonth = 1;
+  const maxMonth = 12;
+  const validateMonth = function (input) {
+    return input < minMonth || input > maxMonth;
+  };
+  const month = STDIN.number(
+    `Enter your month of birthday : only numbers, min: ${minMonth}, max: ${maxMonth}`,
+    validateMonth
+  );
+
+  const minDay = 1;
+  const maxDay = DATA.getMaxDay(year, month);
+  const validateDay = function (input) {
+    return input < minDay || input > maxDay;
+  };
+  const day = STDIN.number(
+    `Enter your day of birthday : only numbers, min: ${minDay}, max: ${maxDay}`,
+    validateDay
+  );
+
+  const user = userFabric(firstName, lastName, year, month, day);
+  console.log(user);
+  entries.push(user);
+  var string =
+    "<section> <div><span>date</span></div></section><section>  <div>    <ul>      <li>        User: <span>firstName</span> <span>lastName</span> -        <span>age</span> years old; </li> </ul> </div></section>";
+
+  var messages = [];
+
+  var div = document.createElement("div");
+  div.innerHTML = string;
+  var spans = div.getElementsByTagName("span");
+  for (var span = 0; span < spans.length; span++) {
+    messages.push(spans[span].innerHTML);
+  }
+  for (let i = 0; i < messages.length; i++) {
+    const message = messages[i];
+    if (message === "firstName") {
+      document.getElementsByTagName("span").value = user.firstName;
+    } else {
+      continue;
+    }
+    console.log(message);
+  }
+
+  console.log(messages);
+}
+addUser();
